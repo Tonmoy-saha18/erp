586d392f74cf29bc8589b660a1db76e29f91d6c7
Tonmoy-Saha-BS23 <tonmoy.saha@brainstation-23.com>
2023-04-17 20:45:57 +0600

    Code added for globalException handler

--- src/main/java/com/brainstation23/erp/exception/custom/GlobalExceptionHandler.java
+++ src/main/java/com/brainstation23/erp/exception/custom/GlobalExceptionHandler.java
@@ -1,10 +1,31 @@
 package com.brainstation23.erp.exception.custom;
 
+import com.brainstation23.erp.exception.custom.custom.AlreadyExistsException;
+import com.brainstation23.erp.exception.custom.custom.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-	// TODO: handle exceptions globally
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex) {
+        log.error("An Exception occurred: ", ex);
+        return new ResponseEntity<>("Internal Server error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleCustomException(NotFoundException ex) {
+        log.error("Not Found Exception occurred: ", ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<String> handleCustomException(AlreadyExistsException ex) {
+        log.error("A custom exception occurred: ", ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
 }
