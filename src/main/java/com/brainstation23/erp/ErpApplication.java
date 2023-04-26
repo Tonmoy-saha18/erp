03bebe1dc50efb5db50802684114b6222021b54b
Tonmoy-saha18 <72729285+Tonmoy-saha18@users.noreply.github.com>
2023-04-26 18:49:13 +0600

    Update ErpApplication.java

--- src/main/java/com/brainstation23/erp/ErpApplication.java
+++ src/main/java/com/brainstation23/erp/ErpApplication.java
@@ -3,6 +3,7 @@ package com.brainstation23.erp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -11,5 +12,11 @@ public class ErpApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ErpApplication.class, args);
 	}
+	
+	@Bean
+    	public OpenAPI openAPI() {
+        	return new OpenAPI()
+                	.addServersItem(new Server().url("/"));
+    	}
 
 }
